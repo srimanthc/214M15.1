a10d8a93c0e2171310c411a3e1bd43d049005372
srimanthc <90794077+srimanthc@users.noreply.github.com>
2025-07-25 15:46:46 -0500

    classes finished, test cases passing

---
+++ app/src/main/java/org/example/Squirrel.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Squirrel {
+    private String name;
+
+    public Squirrel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
+
